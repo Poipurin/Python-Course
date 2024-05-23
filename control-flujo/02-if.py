3b2da87d300b7dec9dac995ffe7db9b8fb469b20
Poipurin <b.pinorojo@gmail.com>
2024-05-23 17:02:52 -0400

    control de flujo

---
+++ control-flujo/02-if.py
@@ -0,0 +1,9 @@
+edad = 60
+if edad > 54:
+    print("Puede ver la película con descuento.")
+elif edad > 17:
+    print("Puede ver la película.")
+else:
+    print("No puede entrar a ver esta película.")
+
+print("Listeilor.")
