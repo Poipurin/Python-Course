d301ca7be2a2ccba717a6a18340c09657daba2d6
Poipurin <b.pinorojo@gmail.com>
2024-05-24 11:11:27 -0400

    control de flujo

---
+++ control-flujo/06-for.py
@@ -0,0 +1,12 @@
+buscar = 10
+for numero in range(5):
+    print(numero)
+    if numero == buscar:
+        print("encontrado", buscar)
+        break 
+else: 
+    print("No se ha enconrado el número buscado")
+
+for char in "Ultimate python":
+    print(char)
+    
---
+++ control-flujo/08-loop-anidado.py
@@ -0,0 +1,3 @@
+for j in range(5):
+    for k in range(2):
+        print(f"{j}, {k}")
