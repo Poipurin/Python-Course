1620c1883ad001d14937d9a9caafe7ad1005fbda
Poipurin <b.pinorojo@gmail.com>
2024-07-01 13:01:27 -0400

    listas

---
+++ tipos-avanzados/05-buscar-elemento.py
@@ -0,0 +1,5 @@
+mascotas = ["Toffee", "Brownie", "Muffin", "Berlín", "Brownie"]
+
+print(mascotas.count("Brownie"))
+if "Brownie" in mascotas:
+    print(mascotas.index("Brownie"))
---
+++ tipos-avanzados/06-agregando-eliminendo.py
@@ -0,0 +1,10 @@
+mascotas = ["Toffee", "Brownie", "Muffin", "Berlín", "Brownie"]
+
+mascotas.insert(1, "Yue")
+mascotas.append("Bosh")
+
+mascotas.remove("Brownie")
+mascotas.pop(3)
+del mascotas[0]
+mascotas.clear()
+print(mascotas)
